effa5233c2e27c1cc2aa3d36b3d69b3ee66f620d
Paulo Abreu <paulo.santana@luby.software>
2022-11-27 00:37:34 -0300

    refactor: 🏗  move sut creation to a function

--- tests/appServices/CreatePostService.spec.ts
+++ tests/appServices/CreatePostService.spec.ts
@@ -2,16 +2,30 @@ import { CreatePostDTO } from '../../src/app/dtos/CreatePostDTO'
 import { CreatePostService } from '../../src/app/services/posts/CreatePostService'
 import { InMemoryRepositoryFactory } from '../../src/shared/infra/factory/InMemoryRepositoryFactory'
 
+interface SutTypes {
+  input: CreatePostDTO
+  sut: CreatePostService
+}
+
+const makeSut = (): SutTypes => {
+  const input = new CreatePostDTO({
+    title: 'any_title',
+    content: 'any_content',
+    authorId: '837ad5dc-8096-4157-80d5-083413ff256a',
+  })
+
+  const fakeRepositoryFactory = new InMemoryRepositoryFactory()
+  const sut = new CreatePostService(fakeRepositoryFactory)
+
+  return {
+    input,
+    sut,
+  }
+}
+
 describe('Create Post Service', () => {
   test('Should create a new post with correct values', async () => {
-    const input = new CreatePostDTO({
-      title: 'any_title',
-      content: 'any_content',
-      authorId: '837ad5dc-8096-4157-80d5-083413ff256a',
-    })
-
-    const fakeRepositoryFactory = new InMemoryRepositoryFactory()
-    const sut = new CreatePostService(fakeRepositoryFactory)
+    const { input, sut } = makeSut()
 
     const result = await sut.execute(input)
 
@@ -22,15 +36,13 @@ describe('Create Post Service', () => {
   })
 
   test('Should throw an error if author not found', async () => {
-    const input = new CreatePostDTO({
-      title: 'any_title',
-      content: 'any_content',
-      authorId: 'any_author_id',
-    })
+    const { sut, input } = makeSut()
 
-    const fakeRepositoryFactory = new InMemoryRepositoryFactory()
-    const sut = new CreatePostService(fakeRepositoryFactory)
+    const inputWithInvalidId = {
+      ...input,
+      authorId: 'invalid_id',
+    }
 
-    await expect(sut.execute(input)).rejects.toThrow()
+    await expect(sut.execute(inputWithInvalidId)).rejects.toThrow()
   })
 })
