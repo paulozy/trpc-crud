2a353ecfa22c6e0f98865141ac51beb4c0e4bb69
Paulo Abreu <paulo.santana@luby.software>
2022-11-27 10:04:50 -0300

    feat: ✨ add delete post service

---
+++ src/app/services/posts/DeletePostService.ts
@@ -0,0 +1,38 @@
+import { AbstractRepositoryFactory } from '../../../domain/factory/AbstractRepositoryFactory'
+import { DeletePostUseCase } from '../../../domain/usecases/Post/DeletePostUseCase'
+import { FindPostByIdUseCase } from '../../../domain/usecases/Post/FindPostByIdUseCase'
+
+interface DeletionOutput {
+  statusCode: number
+  message: string
+}
+
+export class DeletePostService {
+  constructor(private readonly repositoryFactory: AbstractRepositoryFactory) {}
+
+  async execute(id: string): Promise<DeletionOutput> {
+    try {
+      const findPost = new FindPostByIdUseCase(this.repositoryFactory)
+
+      const post = await findPost.findById(id)
+
+      if (!post) {
+        throw new Error('Post not found')
+      }
+
+      const deletePost = new DeletePostUseCase(this.repositoryFactory)
+
+      await deletePost.delete(id)
+
+      return {
+        statusCode: 200,
+        message: 'Post deleted successfully',
+      }
+    } catch (error) {
+      return {
+        statusCode: 400,
+        message: 'Error deleting post',
+      }
+    }
+  }
+}
