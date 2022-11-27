8fd20ec8ec3eb522eabe989b25eaa3a3d44fde42
Paulo Abreu <paulo.santana@luby.software>
2022-11-27 00:32:44 -0300

    test: 🧪 add tests to CreatePostService

---
+++ tests/appServices/CreatePostService.spec.ts
@@ -0,0 +1,23 @@
+import { CreatePostDTO } from '../../src/app/dtos/CreatePostDTO'
+import { CreatePostService } from '../../src/app/services/posts/CreatePostService'
+import { InMemoryRepositoryFactory } from '../../src/shared/infra/factory/InMemoryRepositoryFactory'
+
+describe('Create Post Service', () => {
+  test('Should create a new post with correct values', async () => {
+    const input = new CreatePostDTO({
+      title: 'any_title',
+      content: 'any_content',
+      authorId: '837ad5dc-8096-4157-80d5-083413ff256a',
+    })
+
+    const fakeRepositoryFactory = new InMemoryRepositoryFactory()
+    const sut = new CreatePostService(fakeRepositoryFactory)
+
+    const result = await sut.execute(input)
+
+    expect(result).toHaveProperty('id')
+    expect(result.title).toBe(input.title)
+    expect(result.content).toBe(input.content)
+    expect(result.authorId).toBe(input.authorId)
+  })
+})
