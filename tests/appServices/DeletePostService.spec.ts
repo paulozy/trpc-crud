cd0047bdb2b35b2369a9d898177b91f7284834aa
Paulo Abreu <paulo.santana@luby.software>
2022-11-27 10:28:12 -0300

    test: 🧪 ensure DeletePostService throws if post not found

--- tests/appServices/DeletePostService.spec.ts
+++ tests/appServices/DeletePostService.spec.ts
@@ -22,4 +22,12 @@ describe('Delete Post Service', () => {
     expect(result.statusCode).toBe(200)
     expect(result.message).toBe('Post deleted successfully')
   })
+
+  test('Should return an error if post does not exist', async () => {
+    const { sut } = makeSut()
+    const result = await sut.execute('invalid_id')
+
+    expect(result.statusCode).toBe(400)
+    expect(result.message).toBe('Error deleting post')
+  })
 })
